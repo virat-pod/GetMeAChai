788af35f9a3defd7faba5d640b959c2ccfab2d82
Virat Poddar <viratpoddar2012@gmail.com>
2026-03-20 14:48:28 +0530

    Update route.js

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,9 +1,9 @@
 import connectDB from "@/lib/db/connectDB";
 import payment from "@/lib/models/payment";
+import Notification from "@/lib/models/notification";
 
 export const GET = async (req) => {
   const vercelcron = req.headers.get("x-vercel-cron");
-
   if (!vercelcron) {
     return new Response("Unauthorized", { status: 401 });
   }
@@ -11,10 +11,15 @@ export const GET = async (req) => {
   await connectDB();
 
   const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
-
   await payment.deleteMany({
     done: false,
     createdAt: { $lt: tenMinutesAgo },
   });
+
+  const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
+  await Notification.deleteMany({
+    createdAt: { $lt: oneDayAgo },
+  });
+
   return Response.json({ success: true });
 };
