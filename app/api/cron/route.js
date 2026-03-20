3aacd347ac1c284915fe25e515d0b27a32378921
Virat Poddar <viratpoddar2012@gmail.com>
2026-03-20 14:50:02 +0530

    Update route.js

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -3,10 +3,6 @@ import payment from "@/lib/models/payment";
 import Notification from "@/lib/models/notification";
 
 export const GET = async (req) => {
-  const vercelcron = req.headers.get("x-vercel-cron");
-  if (!vercelcron) {
-    return new Response("Unauthorized", { status: 401 });
-  }
 
   await connectDB();
 
